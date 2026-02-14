3109d6da446f69e0481ae19814cbe2b42469bef6
Vignesh Mariappan <vignesh.mariappan@arrise.com>
2026-02-14 13:27:19 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8000;
+
+// parse JSON bodies
+app.use(express.json());
+
+// root route returns a short message
+app.get('/', (req, res) => {
+    res.send('Hello from Sportz server');
+});
+
+// start server and log URL
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}/`);
+});
+
